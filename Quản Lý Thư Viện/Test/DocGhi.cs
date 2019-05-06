9e954c9c19b0993308f163784c80e45943f031a1
PHDQQ <45413576+PHDQQ@users.noreply.github.com>
2019-05-06 23:26:01 +0700

    PHD

---
+++ Quản Lý Thư Viện/Test/DocGhi.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+
+
+namespace Test
+{
+    //class để đọc ghi file excel
+    class DocGhi
+    {
+        //khởi tại 1 ứng dụng Excel
+        public static Excel._Application App;
+        //khởi tạo bảng tính excel
+        public static Excel._Workbook Workbook;
+        //khởi tạo các sheets trong workbook
+        public static Excel._Worksheet Worksheet;
+        //quản lý các thành phần trong sheets
+        public static Excel.Range Range;
+
+        //hàm để mở file excel
+        public static void open(String fileName)
+        {
+            //tạo đối tượng COM mở kết nối với các đối tượng cần dùng để đọc dữ liệu
+            App = new Excel.Application();
+            Workbook = App.Workbooks.Open(fileName);
+            //chọn sheets đầu tiên
+            Worksheet = Workbook.Sheets[1];
+            Range = Worksheet.UsedRange;
+        }
+
+        //phương thức tạo 1 danh sách mảng đối tượng 1 
+        //chiều mỗi 1 mảng là 1 dòng trong bảng tính Excel
+        public static List<Object[]> getExcelFile(String fileName)
+        {
+            //mở file
+            open(fileName);
+            //đếm số dòng dữ liệu
+            int rowCount = Range.Rows.Count;
+            //khai báo số cột cần lấy giá trị
+            int colCount = 4;
+            //cột bắt đầu đọc dữ liệu từ cột 2 đến cột 4
+            int j = 2;
+            //khởi tạo 1 danh sách mảng đối tượng
+            List<Object[]> list = new List<Object[]>();
+
+            String ten = "";
+            String sl ="";       
+            String tg = "";
+            String mess = "";
+            //đọc từng thành phần trong excel
+            for (int i = 2; i <= 13; i++)
+            {
+
+                try
+                {
+                    ten = Range.Cells[i, j++].Value2.ToString();
+                }
+                catch (Exception)
+                {
+                    ten = "";
+                }
+
+                
+                try
+                {
+                    tg = Range.Cells[i, j++].Value2.ToString();
+                }
+                catch (Exception)
+                {
+                    tg = "";
+                }
+                try
+                {
+                        sl = Range.Cells[i, j++].Value2.ToString();
+                }
+                catch (Exception)
+                {
+                    sl = "";
+                }
+                try
+                {
+                    mess = Range.Cells[i, j++].Value2.ToString();
+                }
+                catch (Exception)
+                {
+                    mess = null;
+                }
+                if (j > colCount) { j = 2; }
+                //thêm dữ liệu vào mảng ta chỉ đọc đến cột số 4 nếu lớn hơn quay lại cột bạn đầu
+                Object[] obj = new Object[] { ten, tg, sl, mess };
+                list.Add(obj);
+
+
+            }
+            //đóng kết nối
+            Workbook.Close();
+            return list;
+        }
+
+        //phương thức dùng để ghi dữ liệu vào file excel
+        public static void setExcelFile(int i, int j, String str, String fileName)
+        {
+            open(fileName);
+            Range.Cells[i, j].value2 = str;
+            Workbook.Save();
+            Workbook.Close();
+        }
+
+
+    }
+
+}
---
+++ Quản Lý Thư Viện/Test/TestSua.cs
@@ -0,0 +1,105 @@
+﻿using QuanLyThuVien;
+using System;
+using NUnit.Extensions.Forms;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+
+
+
+namespace Test
+{
+    [TestFixture]
+    public class TestThemSach
+    {
+        //dòng bắt đầu ghi dữ liệu
+        public int row = 2;
+        //hàm lấy datatest từ file excel
+        public static List<Object[]> data()
+        {
+            return DocGhi.getExcelFile(@"C:\Users\HP\Documents\TCs_Sua1..xlsx");
+        }
+        //lấy dữ liệu đẩy vào testcase
+        [TestCaseSource("data"), Test]
+        public void SuaRound1(String ten, String tg, String sl, String mess)
+        {
+            //khởi tạo đối tượng của form QuanLySach
+            QuanLySach qls = new QuanLySach();
+            //Hiện form
+            qls.Show();
+            //khởi tạo 1 biến để lấy kết quả từ messagebox 
+            string actmsg = "";
+            ModalFormTester msgBox = new ModalFormTester();
+            //phương thức lấy giá trị của messagebox
+            msgBox.ExpectModal("NOTE", delegate
+            {
+                MessageBoxTester ms = new MessageBoxTester("NOTE");
+                actmsg = ms.Text;
+                ms.ClickOk();
+            });
+            // set giá trị cho các trường sau khi lấy dữ liệu từ excel
+            qls.dataTest(ten, tg,sl);
+            //khởi tạo 1 button
+            ButtonTester btnSua = new ButtonTester("btnSuaSach");
+            //click vào button Sửa trên form 
+            btnSua[0].Click();
+            //so sách kết quả lấy từ messagebox với kết quả mong đợi
+            if (mess.Equals(actmsg))
+            {
+                //ghi kết quả vào file excel
+                DocGhi.setExcelFile(row++, 6, "Pass", @"C:\Users\HP\Documents\TCs_Sua1..xlsx");
+                Assert.AreEqual(actmsg, mess);
+            }
+            else
+            {
+                DocGhi.setExcelFile(row++, 6, "Pending", @"C:\Users\HP\Documents\TCs_Sua1..xlsx");
+                Assert.AreEqual(actmsg, mess);
+            }
+            //đóng form sau khi thực hiện xong
+            qls.Close();
+        }
+
+
+        //lấy dữ liệu đẩy vào testcase
+        [TestCaseSource("data"), Test]
+        public void SuaRound2(String ten, String tg, String sl, String mess)
+        {
+            //khởi tạo đối tượng của form QuanLySach
+            QuanLySach qls = new QuanLySach();
+            //Hiện form
+            qls.Show();
+            //khởi tạo 1 biến để lấy kết quả từ messagebox 
+            string actmsg = "";
+            ModalFormTester msgBox = new ModalFormTester();
+            msgBox.ExpectModal("NOTE", delegate
+            {
+                MessageBoxTester ms = new MessageBoxTester("NOTE");
+                actmsg = ms.Text;
+                ms.ClickOk();
+            });
+            // set giá trị cho các trường sau khi lấy dữ liệu từ excel
+            qls.dataTest(ten, tg, sl);
+            //khởi tạo 1 button
+            ButtonTester btnSua = new ButtonTester("btnSuaSach");
+            //click vào button Sửa trên form 
+            btnSua[0].Click();
+            //so sách kết quả lấy từ messagebox với kết quả mong đợi
+            if (mess.Equals(actmsg))
+            {
+                //ghi kết quả vào file excel
+                DocGhi.setExcelFile(row++, 7, "Pass", @"C:\Users\HP\Documents\TCs_Sua1..xlsx");
+                Assert.AreEqual(actmsg, mess);
+            }
+            else
+            {
+                DocGhi.setExcelFile(row++, 7, "Pending", @"C:\Users\HP\Documents\TCs_Sua1..xlsx");
+                Assert.AreEqual(actmsg, mess);
+            }
+            //đóng form sau khi thực hiện xong
+            qls.Close();
+        }
+    }
+
+}
+
+
